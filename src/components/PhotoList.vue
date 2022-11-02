90958f067194dc6d2e28c5d8825392e14c883764
qdwang <wangqidong@gmail.com>
2022-11-02 17:11:39 +0800

    remove unnessary script setup

--- src/components/PhotoList.vue
+++ src/components/PhotoList.vue
@@ -1,25 +1,3 @@
-<script setup>
-import { quickraw } from "../quickraw";
-
-const emit = defineEmits(["raw_decoded"]);
-
-const fileSelected = (info) => {
-    window.timer.file_to_load = performance.now();
-
-    const reader = new FileReader();
-    reader.onload = () => {
-        window.timer.file_loaded = performance.now();
-
-        const img = quickraw.load_image(new Uint8Array(reader.result));
-
-        window.timer.raw_decoded = performance.now();
-
-        emit("raw_decoded", img);
-    };
-    reader.readAsArrayBuffer(info.file.file);
-};
-</script>
-
 <template>
     <div class="flex">
         <n-upload class="flex" :show-file-list="false" directory-dnd @change="fileSelected">
@@ -27,6 +5,30 @@ const fileSelected = (info) => {
         </n-upload>
     </div>
 </template>
+
+<script>
+import { quickraw } from "../quickraw";
+
+export default {
+    methods: {
+        fileSelected(info) {
+            window.timer.file_to_load = performance.now();
+
+            const reader = new FileReader();
+            reader.onload = () => {
+                window.timer.file_loaded = performance.now();
+
+                const img = quickraw.load_image(new Uint8Array(reader.result));
+
+                window.timer.raw_decoded = performance.now();
+
+                this.$emit("raw_decoded", img);
+            };
+            reader.readAsArrayBuffer(info.file.file);
+        }
+    }
+}
+</script>
 
 <style scoped>
 .n-upload-dragger {
