b3594d378f2af9af8951cda1ea042e2bf117636e
qdwang <wangqidong@gmail.com>
2022-11-03 15:09:31 +0800

    add reset to control panel

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -8,29 +8,44 @@
 </template>
 
 <script>
-import { updateUniform, quickraw } from "../quickraw";
+import { updateUniform, quickraw, disposeWasm } from "../quickraw";
 
 let timeout = 1;
 const lag = 300;
 
 export default {
-    props: ['webgl_instance'],
+    props: ['webgl_instance', 'timer'],
     data() {
         return {
+            is_reset: false,
             gamma: 2.22,
             exposure: 0
         }
     },
     watch: {
+        'timer.histogram_calced'() {
+            this.is_reset = true;
+            this.gamma = 2.22;
+            this.exposure = 0;
+            this.$nextTick(() => {
+                this.is_reset = false;
+            });
+        },
         exposure(v) {
+            if (this.is_reset) return;
+
             timeout = updateUniform(this.webgl_instance, 'uniform1f', 'exposure', v, pixels => {
                 const histogram_data = quickraw.calc_histogram(pixels);
+                disposeWasm();
                 this.$emit("histogram_load", histogram_data);
             }, timeout, lag);
         },
         gamma(v) {
+            if (this.is_reset) return;
+
             timeout = updateUniform(this.webgl_instance, 'uniform1f', 'gamma', 1 / v, pixels => {
                 const histogram_data = quickraw.calc_histogram(pixels);
+                disposeWasm();
                 this.$emit("histogram_load", histogram_data);
             }, timeout, lag);
         }
