80981b9b8ca30eb1c1f84010b2180e0822fe923d
qdwang <wangqidong@gmail.com>
2022-11-05 13:24:29 +0800

    add several sliders

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -5,7 +5,15 @@
         exposure {{ exposure }}
         <o-slider v-model="exposure" :step="0.01" :min="-3" :max="3" :tooltip="false" />
         white point {{ white_point }}
-        <o-slider v-model="white_point" :step="0.01" :min="-1" :max="1" :tooltip="false" />
+        <o-slider v-model="white_point" :step="0.01" :min="-1" :max="1.5" :tooltip="false" />
+        black point {{ black_point }}
+        <o-slider v-model="black_point" :step="0.01" :min="-1" :max="1" :tooltip="false" />
+        highlight {{ highlight_point }}
+        <o-slider v-model="highlight_point" :step="0.01" :min="-1" :max="1" :tooltip="false" />
+        shadow {{ shadow_point }}
+        <o-slider v-model="shadow_point" :step="0.01" :min="-1" :max="1" :tooltip="false" />
+        highlight threshold {{ highlight_threshold }}
+        <o-slider v-model="highlight_threshold" :step="0.01" :min="0" :max="1" :tooltip="false" />
     </div>
 </template>
 
@@ -23,6 +31,21 @@ export default {
             gamma: 2.22,
             exposure: 0,
             white_point: 0,
+            black_point: 0,
+            highlight_point: 0,
+            shadow_point: 0,
+            highlight_threshold: 0.75
+        }
+    },
+    methods: {
+        setShader(name, value) {
+            if (this.is_reset) return;
+
+            timeout = updateUniform(this.webgl_instance, 'uniform1f', name, value, pixels => {
+                const histogram_data = quickraw.calc_histogram(pixels);
+                disposeWasm();
+                this.$emit("histogram_load", histogram_data);
+            }, timeout, lag);
         }
     },
     watch: {
@@ -31,37 +54,35 @@ export default {
             this.gamma = 2.22;
             this.exposure = 0;
             this.white_point = 0;
+            this.black_point = 0;
+            this.highlight_point = 0;
+            this.shadow_point = 0;
+            this.highlight_threshold = 0.75;
             this.$nextTick(() => {
                 this.is_reset = false;
             });
         },
         exposure(v) {
-            if (this.is_reset) return;
-
-            timeout = updateUniform(this.webgl_instance, 'uniform1f', 'exposure', v, pixels => {
-                const histogram_data = quickraw.calc_histogram(pixels);
-                disposeWasm();
-                this.$emit("histogram_load", histogram_data);
-            }, timeout, lag);
+            this.setShader('exposure', v);
         },
         gamma(v) {
-            if (this.is_reset) return;
-
-            timeout = updateUniform(this.webgl_instance, 'uniform1f', 'gamma', 1 / v, pixels => {
-                const histogram_data = quickraw.calc_histogram(pixels);
-                disposeWasm();
-                this.$emit("histogram_load", histogram_data);
-            }, timeout, lag);
+            this.setShader('gamma', 1/ v);
         },
         white_point(v) {
-            if (this.is_reset) return;
-
-            timeout = updateUniform(this.webgl_instance, 'uniform1f', 'white_point', v, pixels => {
-                const histogram_data = quickraw.calc_histogram(pixels);
-                disposeWasm();
-                this.$emit("histogram_load", histogram_data);
-            }, timeout, lag);
-        }
+            this.setShader('white_point', v);
+        },
+        black_point(v) {
+            this.setShader('black_point', v);
+        },
+        highlight_point(v) {
+            this.setShader('highlight_point', v);
+        },
+        shadow_point(v) {
+            this.setShader('shadow_point', v);
+        },
+        highlight_threshold(v) {
+            this.setShader('highlight_threshold', v);
+        },
     }
 }
 </script>
