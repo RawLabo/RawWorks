3e11fc4db3d5894499e9d7b5a7857928616c2c1a
qdwang <wangqidong@gmail.com>
2022-11-27 16:33:17 +0800

    remove oruga upload

--- src/main.js
+++ src/main.js
@@ -5,13 +5,12 @@ import init, * as quickraw from './quickraw/quickraw'
 
 const app = createApp(App);
 
-import { OSlider, OUpload, OCheckbox, OButton } from '@oruga-ui/oruga-next';
+import { OSlider, OCheckbox, OButton } from '@oruga-ui/oruga-next';
 import '@oruga-ui/oruga-next/dist/oruga-full.css';
 
 app
     .component('OButton', OButton)
     .component('OSlider', OSlider)
-    .component('OUpload', OUpload)
     .component('OCheckbox', OCheckbox);
 
 app.mount('#app');
