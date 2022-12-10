0435de4cfc726fc3ef82eabca83bf03098173a5a
qdwang <wangqidong@gmail.com>
2022-12-10 22:13:01 +0800

    add app info

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  define: {
+    '__APP_INFO__': JSON.stringify(process.env.npm_package_name + ' v' + process.env.npm_package_version),
+  },
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
