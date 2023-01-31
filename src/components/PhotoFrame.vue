b437d9ac7e8928d36855c7f369d03f0b15819d2c
qdwang <wangqidong@gmail.com>
2023-01-31 09:49:08 +0800

    update arrow icon

--- src/components/PhotoFrame.vue
+++ src/components/PhotoFrame.vue
@@ -9,7 +9,12 @@
     }"></canvas>
     <div class="tip" v-if="img">{{ Math.round(tip.scale * 100) }}%</div>
     <button :class="{ 'edit-btn': true, reverse: ui_edit_reverse }"
-      @click="$emit('toggle_control'); ui_edit_reverse = !ui_edit_reverse">⬅</button>
+      @click="$emit('toggle_control'); ui_edit_reverse = !ui_edit_reverse">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path
+          d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256S114.6 512 256 512s256-114.6 256-256zM215 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L392 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-214.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L103 273c-9.4-9.4-9.4-24.6 0-33.9L215 127z" />
+      </svg>
+    </button>
   </div>
 </template>
 
@@ -230,22 +235,23 @@ export default {
 @media only screen and (max-width: 600px) {
   body .edit-btn {
     display: block;
+    width: 16px;
+    height: 16px;
+    fill: #aaaa;
   }
 }
 
 .edit-btn {
   display: none;
   position: absolute;
-  bottom: 0;
-  right: 4px;
+  bottom: 2px;
+  right: 2px;
   border: 0;
   background: transparent;
-  font-weight: 400;
   cursor: pointer;
-  font-size: 16px;
-  color: #aaaa;
   transition: transform ease 0.2s;
 }
+
 .edit-btn.reverse {
   transform: rotate(180deg);
 }
